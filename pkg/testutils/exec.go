e61d8d724f5d0c03948d17047bce2f2309d11b98
Christian Fritz <mail@chr-fritz.de>
2019-05-14 07:45:03 +0200

    Adds an improved and common usable mock process implementation for testing external command calls.

--- pkg/testutils/exec.go
+++ pkg/testutils/exec.go
@@ -1,9 +1,14 @@
 package testutils
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
 	"os"
 	"os/exec"
+	"reflect"
+	"testing"
+	"time"
 )
 
 // This command fakes the exec command. This should be only used in Tests.
@@ -12,6 +17,13 @@ func FakeExecCommand(command string, args ...string) *exec.Cmd {
 	cs = append(cs, args...)
 	cmd := exec.Command(os.Args[0], cs...)
 	cmd.Env = []string{"GO_WANT_HELPER_PROCESS=1"}
+
+	if len(TestProcessResponses) > 0 {
+		var buf bytes.Buffer
+		encoder := json.NewEncoder(&buf)
+		_ = encoder.Encode(TestProcessResponses)
+		cmd.Env = append(cmd.Env, "RESPONSE="+buf.String())
+	}
 	return cmd
 }
 
@@ -31,3 +43,52 @@ func ExtractMockedCommandAndArgs() (string, []string) {
 	}
 	return args[0], args[1:]
 }
+
+func StandardHelperProcess(*testing.T) {
+	if os.Getenv("GO_WANT_HELPER_PROCESS") != "1" {
+		return
+	}
+
+	strResponse := os.Getenv("RESPONSE")
+	var responses []TestProcessResponse
+	if strResponse != "" {
+		decoder := json.NewDecoder(bytes.NewBufferString(strResponse))
+		_ = decoder.Decode(&responses)
+	}
+
+	executedCommand, executedArgs := ExtractMockedCommandAndArgs()
+
+	response := findTestProcessResponse(responses, executedCommand, executedArgs)
+
+	defer os.Exit(response.ResponseStatus)
+	_, _ = fmt.Fprint(os.Stderr, response.Stderr)
+	_, _ = fmt.Fprint(os.Stdout, response.Stdout)
+	time.Sleep(response.delay)
+}
+
+func findTestProcessResponse(responses []TestProcessResponse, cmd string, args []string) *TestProcessResponse {
+	for _, response := range responses {
+		if response.Command != cmd {
+			continue
+		}
+		if !reflect.DeepEqual(response.Args, args) {
+			continue
+		}
+		return &response
+	}
+	return &TestProcessResponse{
+		Stderr:         fmt.Sprintf("No matching response found for command line: \"%s %s\"", cmd, args),
+		ResponseStatus: -2,
+	}
+}
+
+type TestProcessResponse struct {
+	Command        string
+	Args           []string
+	ResponseStatus int
+	Stdout         string
+	Stderr         string
+	delay          time.Duration
+}
+
+var TestProcessResponses []TestProcessResponse
