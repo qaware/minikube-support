b555f02aeea9234d4db6bef8d3e4551cc2077adf
Christian Fritz <christian.fritz@qaware.de>
2019-07-05 09:49:23 +0200

    Logs all process calls to trace.

--- pkg/sh/exec.go
+++ pkg/sh/exec.go
@@ -1,13 +1,15 @@
 package sh
 
 import (
+	"github.com/sirupsen/logrus"
 	"os"
 	"os/exec"
+	"strings"
 )
 
 // Alias for os/exec.Command to allow mocking the command executions in tests.
 // see https://npf.io/2015/06/testing-exec-command/ for more information about.
-var ExecCommand = exec.Command
+var ExecCommand = executeCommand
 
 // Executes echo with sudo and connected stdin, stdout and stderr to ask for the users password.
 // This allows to use sudo for commands which internal process stdout and stderr which are not connected to the current tty.
@@ -17,9 +19,16 @@ func InitSudo() error {
 	return command.Run()
 }
 
+// RunCmd executes the given command including the current environment and returns the combined output as string.
 func RunCmd(command string, args ...string) (string, error) {
 	cmd := ExecCommand(command, args...)
 	cmd.Env = append(cmd.Env, os.Environ()...)
 	bytes, e := cmd.CombinedOutput()
 	return string(bytes), e
+}
+
+// executeCommand is a wrapper for exec.Command() to log the executed command.
+func executeCommand(name string, arg ...string) *exec.Cmd {
+	logrus.Tracef("Executing command: %s %s", name, strings.Join(arg, " "))
+	return exec.Command(name, arg...)
 }
