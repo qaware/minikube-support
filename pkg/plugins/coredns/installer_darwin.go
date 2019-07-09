28545da70d7fe3a054dc1a15edfa1c014387d7de
Christian Fritz <christian.fritz@qaware.de>
2019-07-09 11:48:45 +0200

    Adds resolver config under macOS for .minikube.

--- pkg/plugins/coredns/installer_darwin.go
+++ pkg/plugins/coredns/installer_darwin.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"github.com/kballard/go-shellquote"
 	"github.com/qaware/minikube-support/pkg/sh"
+	"github.com/sirupsen/logrus"
 	"os"
 )
 
-const launcctlConfig = "/Library/LaunchDaemons/de.chrfritz.minikube-support.coredns.plist"
+const launchctlConfig = "/Library/LaunchDaemons/de.chrfritz.minikube-support.coredns.plist"
+const dotMinikubeResolverPath = "/etc/resolver/minikube"
 
 func (i *installer) installSpecific() error {
 	e := sh.InitSudo()
@@ -19,24 +21,30 @@ func (i *installer) installSpecific() error {
 	if e != nil {
 		return fmt.Errorf("can not write launchctl config: %s", e)
 	}
-	_, e = sh.RunCmd("sudo", "launchctl", "load", launcctlConfig)
+	_, e = sh.RunCmd("sudo", "launchctl", "load", launchctlConfig)
 	if e != nil {
 		return fmt.Errorf("can not load coredns launch daemon: %s", e)
 	}
-	return nil
+
+	return i.writeResolverConfig()
 }
 func (i *installer) updateSpecific() error {
-
 	return nil
 }
 func (i *installer) uninstallSpecific() error {
-	_, e := sh.RunCmd("sudo", "launchctl", "unload", launcctlConfig)
+	_, e := sh.RunCmd("sudo", "launchctl", "unload", launchctlConfig)
 	if e != nil {
 		return fmt.Errorf("can not unload coredns launch daemon: %s", e)
 	}
-	_, e = sh.RunCmd("sudo", "rm", launcctlConfig)
+
+	_, e = sh.RunCmd("sudo", "rm", launchctlConfig)
 	if e != nil {
 		return fmt.Errorf("can not remove coredns launch daemon config: %s", e)
+	}
+
+	_, e = sh.RunCmd("sudo", "rm", dotMinikubeResolverPath)
+	if e != nil {
+		return fmt.Errorf("can not remove coredns minikube resolver config: %s", e)
 	}
 	return nil
 }
@@ -71,26 +79,41 @@ func (i *installer) writeLaunchCtlConfig() error {
 </plist>
 `
 
-	command := sh.ExecCommand("sudo", "/bin/sh", "-c", shellquote.Join("sed", "-n", "w "+launcctlConfig))
+	return i.writeFileAsRoot(launchctlConfig, []byte(config))
+}
+
+func (i *installer) writeResolverConfig() error {
+	config := "nameserver ::1"
+	return i.writeFileAsRoot(dotMinikubeResolverPath, []byte(config))
+}
+
+func (i *installer) writeFileAsRoot(path string, content []byte) error {
+	command := sh.ExecCommand("sudo", "/bin/sh", "-c", shellquote.Join("sed", "-n", "w "+path))
 	command.Env = append(command.Env, os.Environ()...)
-	defer command.Wait()
+	defer func() {
+		if e := command.Wait(); e != nil {
+			logrus.Errorf("Unable to wait for writing %s: %s", path, e)
+		}
+	}()
+
 	writer, e := command.StdinPipe()
 	if e != nil {
-		return fmt.Errorf("write launchctl config failed: %s", e)
+		return fmt.Errorf("write content into %s failed: %s", path, e)
 	}
-	_, e = writer.Write([]byte(config))
+
+	_, e = writer.Write(content)
 	if e != nil {
-		return fmt.Errorf("write launchctl config failed: %s", e)
+		return fmt.Errorf("write content into %s failed: %s", path, e)
 	}
+
 	e = command.Start()
 	if e != nil {
-		return fmt.Errorf("write launchctl config failed: %s", e)
+		return fmt.Errorf("write content into %s failed: %s", path, e)
 	}
 
 	e = writer.Close()
-
 	if e != nil {
-		return fmt.Errorf("write launchctl config failed: %s", e)
+		return fmt.Errorf("write content into %s failed: %s", path, e)
 	}
 	return nil
 }
