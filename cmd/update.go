6ed48cf889329a061539552c7598820b2646338a
Christian Fritz <mail@chr-fritz.de>
2020-11-07 18:54:31 +0100

    Allow to update the cluster plugins only.

--- cmd/update.go
+++ cmd/update.go
@@ -7,7 +7,8 @@ import (
 )
 
 type UpdateOptions struct {
-	registry apis.InstallablePluginRegistry
+	registry            apis.InstallablePluginRegistry
+	includeLocalPlugins bool
 }
 
 func NewUpdateOptions(registry apis.InstallablePluginRegistry) *UpdateOptions {
@@ -21,16 +22,21 @@ func NewUpdateCommand(registry apis.InstallablePluginRegistry) *cobra.Command {
 
 	command := &cobra.Command{
 		Use:   "update",
-		Short: "Updates all or one of the available plugins.",
-		Run:   options.Run,
+		Short: "Updates the cluster plugins.",
+		Long: "The update command updates at least all cluster plugins. If you add the -l or --updateLocal flag " +
+			"it will also update the local plugins.",
+		Run: options.Run,
 	}
 	command.AddCommand(CreateUpdateCommands(options.registry)...)
+	command.Flags().BoolVarP(&options.includeLocalPlugins, "uninstallLocal", "l", false, "Also remove the local plugins.")
 	return command
-
 }
 
 func (i *UpdateOptions) Run(cmd *cobra.Command, args []string) {
 	for _, plugin := range i.registry.ListPlugins() {
+		if !i.includeLocalPlugins && apis.IsLocalPlugin(plugin) {
+			continue
+		}
 		logrus.Info("Update plugin:", plugin)
 		plugin.Update()
 	}
--- cmd/update_test.go
+++ cmd/update_test.go
@@ -1,6 +1,7 @@
 package cmd
 
 import (
+	"github.com/qaware/minikube-support/pkg/apis"
 	"testing"
 
 	"github.com/qaware/minikube-support/pkg/plugins"
@@ -10,14 +11,34 @@ import (
 )
 
 func TestUpdateOptions_Run(t *testing.T) {
-	plugin := &DummyPlugin{}
-	registry := plugins.NewInstallablePluginRegistry()
-	registry.AddPlugin(plugin)
-	i := UpdateOptions{
-		registry: registry,
+	tests := []struct {
+		name                string
+		phase               apis.Phase
+		includeLocalPlugins bool
+		updated             bool
+	}{
+		{"cluster plugin without local", apis.CLUSTER_TOOLS_INSTALL, false, true},
+		{"cluster plugin with local", apis.CLUSTER_TOOLS_INSTALL, true, true},
+		{"local plugin without local", apis.LOCAL_TOOLS_INSTALL, false, false},
+		{"local plugin with local", apis.LOCAL_TOOLS_INSTALL, true, true},
 	}
-	i.Run(&cobra.Command{}, []string{})
-	assert.False(t, plugin.installRun)
-	assert.True(t, plugin.updateRun)
-	assert.False(t, plugin.uninstallRun)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			plugin := &DummyPlugin{
+				phase: tt.phase,
+			}
+			registry := plugins.NewInstallablePluginRegistry()
+			registry.AddPlugin(plugin)
+			i := UpdateOptions{
+				registry:            registry,
+				includeLocalPlugins: tt.includeLocalPlugins,
+			}
+
+			i.Run(&cobra.Command{}, []string{})
+
+			assert.False(t, plugin.installRun)
+			assert.Equal(t, tt.updated, plugin.updateRun)
+			assert.False(t, plugin.uninstallRun)
+		})
+	}
 }
