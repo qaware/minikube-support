8d986dfd19edf3db7b624c668ffb55db39f7ac9f
Christian Fritz <christian.fritz@qaware.de>
2020-11-30 12:09:13 +0100

    Fixes log message when installing plugins.

--- cmd/install.go
+++ cmd/install.go
@@ -1,9 +1,10 @@
 package cmd
 
 import (
-	"github.com/qaware/minikube-support/pkg/apis"
 	"github.com/sirupsen/logrus"
 	"github.com/spf13/cobra"
+
+	"github.com/qaware/minikube-support/pkg/apis"
 )
 
 type InstallOptions struct {
@@ -37,11 +38,11 @@ func NewInstallCommand(registry apis.InstallablePluginRegistry) *cobra.Command {
 
 func (i *InstallOptions) Run(cmd *cobra.Command, args []string) {
 	for _, plugin := range i.registry.ListPlugins() {
-		logrus.Info("Install plugin:", plugin)
 		if !i.includeLocalPlugins && apis.IsLocalPlugin(plugin) {
 			continue
 		}
 
+		logrus.Info("Install plugin:", plugin)
 		plugin.Install()
 	}
 }
