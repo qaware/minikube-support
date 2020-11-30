c5b5710fd70140be5bbe93a1eba67669239908db
Christian Fritz <christian.fritz@qaware.de>
2020-11-30 12:39:14 +0100

    Adds default config for unix systems.

--- pkg/plugins/coredns/installer_unix.go
+++ pkg/plugins/coredns/installer_unix.go
@@ -2,6 +2,28 @@
 
 package coredns
 
-func (i *installer) installSpecific() error   { return nil }
-func (i *installer) uninstallSpecific() error { return nil }
-func (i *installer) writeConfig() error       { return nil }
+func (i *installer) installSpecific() error {
+	// nothing to do at the moment
+	return nil
+}
+func (i *installer) uninstallSpecific() error {
+	// nothing to do at the moment
+	return nil
+}
+func (i *installer) writeConfig() error {
+	config := `
+. {
+    reload
+    health :8054
+    bind 127.0.0.1 
+    bind ::1
+    log
+
+    grpc minikube 127.0.0.1:8053
+}
+192.168.64.1:53  {
+    forward . /etc/resolv.conf
+}
+`
+	return ioutil.WriteFile(i.prefix.coreFile(), []byte(config), 0644)
+}
